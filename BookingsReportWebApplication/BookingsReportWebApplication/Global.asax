9433b31f332f297fdc47484baaef318c7d10ca99
neoobject <support@neoobject.com>
2016-10-11 21:31:46 -0600

    Initial Check In

---
+++ BookingsReportWebApplication/BookingsReportWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookingsReportWebApplication.MvcApplication" Language="C#" %>
---
+++ BookingsReportWebApplication/BookingsReportWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookingsReportWebApplication.Startup))]
+namespace BookingsReportWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
